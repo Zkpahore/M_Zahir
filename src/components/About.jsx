000e92b4658b5de20116c4ef02c46c24e653e137
zkpahore <zahirpahore@yahoo.com>
2024-03-03 21:10:11 +0500

    first commihft

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,27 +3,34 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const About = () => {
+  const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#8811ff']; 
+
+  const items = ['Html_5', 'React_Js', 'Tailwind_Css', 'Css', 'ES6', 'Vite_frame', 'React_Router'];
+  const iterm = ['Express_Js', 'Node_Js', 'Mongoose', 'Rest_API', 'MongoDB'];
+
   return (
+    
+    <div className='md:flex lg:flex gap-4'>
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
       transition={{ duration: 0.5 }}
-      className="section p-4 bg-gray-100 rounded-lg shadow-md w-3/4"
+      className="section p-4 bg-gray-100 rounded-lg shadow-md w-full md:w-3/6 lg:w-3/6 flex flex-wrap"
     >
       <h2 className="text-2xl font-bold mb-4">About Me</h2>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.2, duration: 0.5 }}
-        className="card flex items-center"
+        className="card items-center"
       >
         <div className="profile-image mr-4">
           <motion.img
             initial={{ scale: 0.8 }}
             animate={{ scale: 1 }}
             transition={{ delay: 0.4, duration: 0.5, type: 'spring', stiffness: 100 }}
-            src="https://media.licdn.com/dms/image/D4D03AQH-WDctQw5jGw/profile-displayphoto-shrink_800_800/0/1706357365959?e=2147483647&v=beta&t=veY8RUuWQ0rmyn81wkeH5GLdbPBNqhYOiwBSGJ7i8GI"
+            src="zkpahore.jpg"
             alt="Profile"
             className="rounded-full w-24 h-24 object-cover"
           />
@@ -33,9 +40,9 @@ const About = () => {
             initial={{ opacity: 0, x: -20 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: 0.6, duration: 0.5 }}
-            className="text-lg font-bold mb-1"
+            className="text-lg font-bold my-4"
           >
-            John Doe
+            Muhammad Zahir
           </motion.h3>
           <motion.p
             initial={{ opacity: 0, x: -20 }}
@@ -51,39 +58,93 @@ const About = () => {
             transition={{ delay: 1, duration: 0.5 }}
             className="social-icons flex mt-2"
           >
-            <motion.a
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.8 }}
+            <div
               href="#"
               target="_blank"
               rel="noopener noreferrer"
               className="mr-2"
             >
-              <i className="fab fa-linkedin-in text-blue-500"></i>
-            </motion.a>
-            <motion.a
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.8 }}
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mr-2"
-            >
-              <i className="fab fa-github text-gray-800"></i>
-            </motion.a>
-            <motion.a
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.8 }}
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-twitter text-blue-400"></i>
-            </motion.a>
+            <p>Hello, I'm Muhammad Zahir, a MERN (MongoDB, Express.js, React.js, Node.js)
+             stack developer passionate about building robust and scalable web applications.
+             With proficiency in both frontend and backend technologies,
+             I specialize in creating dynamic and responsive web solutions that meet client needs and exceed expectations.
+             From database design and API development to user interface implementation,
+              I leverage the power of the MERN stack to deliver efficient and user-friendly applications.</p>
+            </div>
           </motion.div>
         </div>
       </motion.div>
     </motion.div>
+
+<hr className='block md:hidden lg:hidden border-4'/>
+
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.5 }}
+      className="section p-2 bg-gray-100 rounded-lg shadow-md w-full md:w-3/6 lg:w-3/6 flex flex-wrap"
+    >
+      <h2 className="text-2xl font-bold my-1 mx-2">Skills</h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
+        className="card items-center"
+      >
+        <br />
+       
+        <div className="details">
+          <motion.h3
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.6, duration: 0.5 }}
+            className="text-lg font-bold my-4">
+            Front_End
+          </motion.h3>
+          
+          <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 1, duration: 0.5 }}
+            className="social-icons flex mt-2">
+        <ol className='flex flex-wrap'>
+        {items.map((item, index) => (
+          <li key={index} style={{ borderColor: colors[index % colors.length] }} className="border-2 p-2 m-2 rounded-lg bg-slate-700 text-white">
+            {item}
+          </li>
+        ))}
+      </ol>
+            
+          </motion.div>
+        </div>
+        <div className="details">
+          <motion.h3
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.6, duration: 0.5 }}
+            className="text-lg font-bold my-4">
+            Back_End
+          </motion.h3>
+          
+          <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 1, duration: 0.5 }}
+            className="social-icons flex mt-2">
+        <ol className='flex flex-wrap'>
+        {iterm.map((item, index) => (
+          <li key={index} style={{ borderColor: colors[index % colors.length] }} className="border-2 p-2 m-2 rounded:md bg-slate-700 text-white">
+            {item}
+          </li>
+        ))}
+      </ol>
+            
+          </motion.div>
+        </div>
+      </motion.div>
+    </motion.div>
+    </div>
   );
 };
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion'; // Import motion from Framer Motion
 import Dash from './Dashboard';
 import Serv from './Services';
 import MyPrt from './MyPort';
@@ -6,20 +7,37 @@ import Img from './Img';
 import Con from './Contact';
 import Footer from './Footer';
 import '../App.css';
+
+const nameVariants = {
+  hidden: { opacity: 0, x: -20 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.8,
+      delay: 0.2,
+    },
+  },
+};
+
+const textVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      delay: 0.4,
+    },
+  },
+};
+
 export default function App() {
-  // State to store the current name
   const [name, setName] = useState("Muhammad Zahir");
-
-  // List of names to rotate
   const names = ["Muhammad Zahir", "Web Designer", "Mern stack Developer"];
-
-  // Index to track the current name
   const [currentIndex, setCurrentIndex] = useState(0);
-
-  // Index to track the current character being displayed
   const [currentCharIndex, setCurrentCharIndex] = useState(0);
 
-  // Function to update the name
   const updateName = () => {
     setCurrentCharIndex((prevIndex) => (prevIndex + 1) % (names[currentIndex].length + 1));
     if (currentCharIndex === 0) {
@@ -38,22 +56,26 @@ export default function App() {
 
   return (
     <>
-    <div className='bg-slate-900 w-full  block md:flex lg:flex text-white'>
-      <div className='w-full md:w-1/2 lg:w-1/2  flex-shrink-0 gap-10 lg:m-24'>
-        <h2 className='text-2xl lg:text-5xl  font-extrabold ml-6 py-2'>Hello!<br />I Am {name}<span className='text-gray-400'>|</span></h2>
-        <p className='p-2 mt-6 '>I'm a Web Developer with extensive experience for over 2 years. My expertise is to create Front-End & Back-End of a web application</p>
-        <button className='mt-10 ml-4 items-center bg-slate-900 text-white p-3 border border-white rounded-full hover:bg-blue-800 font-bold'>Contact Me</button>
+      <div className='bg-slate-900 w-full block md:flex lg:flex text-white'>
+        <div className='w-full md:w-1/2 lg:w-1/2 flex-shrink-0 gap-10 lg:m-24'>
+          <motion.h2 className='text-2xl lg:text-5xl font-extrabold ml-6 py-2' variants={nameVariants} initial="hidden" animate="visible">
+            Hello!<br />I Am {name}<span className='text-gray-400'>|</span>
+          </motion.h2>
+          <motion.p className='p-2 mt-6' variants={textVariants} initial="hidden" animate="visible">
+            I'm a Web Developer with extensive experience for over 2 years. My expertise is to create Front-End & Back-End of a web application
+          </motion.p>
+          <button className='mt-10 ml-4 items-center bg-slate-900 text-white p-3 border border-white rounded-full hover:bg-blue-800 font-bold'>Contact Me</button>
+        </div>
+        <div className='lg:flex w-full md:w-1/2 lg:w-1/2 m-0'>
+          <img src="zkpo.png" alt="Zkpahore" />
+        </div>
       </div>
-      <div className='lg:flex w-full md:w-1/2 lg:w-1/2 m-0'>
-        <img src="zkpo.png" alt="Zkpahore" />
-      </div>
-    </div>
-    <Dash />
-    <Serv />
-    <Img />
-    <MyPrt />
-    <Con />
-    <Footer />
+      <Dash />
+      <Serv />
+      <Img />
+      <MyPrt />
+      <Con />
+      <Footer />
     </>
   );
 }
